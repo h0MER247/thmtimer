e798e5de016300442c35ef61f782141273c42cc3
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2014-04-28 16:53:02 +0200

    LoginActivity

---
+++ src/de/thm/mni/thmtimer/LoginActivity.java
@@ -0,0 +1,14 @@
+package de.thm.mni.thmtimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		this.setContentView(R.layout.loginactivity);
+	}
+}
