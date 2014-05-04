d2d3f9c0467f328700ad62fbfa4d681f38fcf8cb
Sebastian Hübner <sebastian.huebner@mni.thm.de>
2014-05-04 03:28:53 +0200

    GUI Layout für Dozenten erstellt
    (Vorläufig, da ich nicht sicher bin was ein Dozent überhaupt in seiner
    Modulliste angezeigt bekommen soll)
    
    - Der "Rollentausch" eines Dozenten zwischen seiner Dozenten und
    Studenten Rolle wird über Tabs in "TeacherModuleListActivity" erreicht.
    
    - Zwei unterschiedliche Layouts für ListView Einträge (Student:
    studentmodulelistitem.xml, Dozent: teachermodulelistitem.xml)
    
    - Bisschen Code um auch zu sehen ob das ganze Gelepp überhaupt
    funktioniert (siehe TeacherModuleListActivity.java, ModuleData.java)
    
    mfg

---
+++ src/de/thm/mni/thmtimer/ModuleData.java
@@ -0,0 +1,66 @@
+package de.thm.mni.thmtimer;
+
+// -----------------------------------------------------------------------------------
+// Sebastian: Das hier ist erstmal vorläufig damit ich irgendwas anzeigen lassen kann.
+//            Es wird im Endeffekt sowieso ganz anders gemacht ^^
+// -----------------------------------------------------------------------------------
+public class ModuleData {	
+
+	private int     m_id;             // ModulID
+	private String  m_name;           // Name
+	private int     m_studentCount;   // Anzahl Studenten in diesem Modul
+	private String  m_teacher;        // Dozent
+	private String  m_semester;       // Semester
+	private String  m_timeInvested;   // Gesamte investierte Zeit in dieses Modul
+	private boolean m_timeLogRunning; // Zeiterfassung läuft (Ja / Nein)
+	
+	
+	
+	public ModuleData(int id, String name, int studentCount, String teacher, String semester, String timeInvested, boolean timeLogRunning) {
+		
+		m_id = id;
+		m_name = name;
+		m_studentCount = studentCount;
+		m_teacher = teacher;
+		m_semester = semester;
+		m_timeInvested = timeInvested;
+		m_timeLogRunning = timeLogRunning;
+	}
+	
+	
+	
+	public int getID() {
+		
+		return m_id;
+	}
+	
+	public String getName() {
+		
+		return m_name;
+	}
+	
+	public int getStudentCount() {
+		
+		return m_studentCount;
+	}
+	
+	public String getTeacher() {
+		
+		return m_teacher;
+	}
+	
+	public String getSemester() {
+		
+		return m_semester;
+	}
+	
+	public String getTimeInvested() {
+		
+		return m_timeInvested;
+	}
+	
+	public boolean getTimeLogRunning() {
+		
+		return m_timeLogRunning;
+	}
+}
