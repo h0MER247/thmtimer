37544ab3661643b8436430f18b2cf13746be8052
Lukas <lukas.schaller@mni.thm.de>
2014-05-04 13:05:21 +0200

    LoginActivity is now able to open TeacherModuleListActivity

--- src/de/thm/mni/thmtimer/LoginActivity.java
+++ src/de/thm/mni/thmtimer/LoginActivity.java
@@ -1,7 +1,11 @@
 package de.thm.mni.thmtimer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -10,5 +14,17 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		this.setContentView(R.layout.loginactivity);
+		
+		Button btnLogin = (Button) findViewById(R.id.btn_login);
+		btnLogin.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(LoginActivity.this,
+						TeacherModuleListActivity.class);
+				startActivity(intent);
+			}
+			
+		});
 	}
 }
