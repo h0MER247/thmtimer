57b701a2b632cae0baca620e77ae51deff3e4b55
Sebastian Hübner <sebastian.huebner@mni.thm.de>
2014-05-14 05:28:06 +0200

    Added Missing Files

---
+++ src/de/thm/mni/thmtimer/model/TimeStatisticData.java
@@ -0,0 +1,28 @@
+package de.thm.mni.thmtimer.model;
+
+
+
+public class TimeStatisticData {
+	
+	private Long m_categoryID;
+	private TimeData m_time;
+	
+	
+	public TimeStatisticData(Long categoryID, TimeData time) {
+		
+		m_categoryID = categoryID;
+		m_time = time;
+	}
+	
+	
+	
+	public Long getCategoryID() {
+		
+		return m_categoryID;
+	}
+	
+	public TimeData getTime() {
+		
+		return m_time;
+	}
+}
