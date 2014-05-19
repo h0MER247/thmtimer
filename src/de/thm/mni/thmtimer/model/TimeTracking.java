bff7c274d9a833d332a2b2caa85b19fc84f4569a
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2014-05-19 12:11:01 +0200

    Remove description from TimeTracking

--- src/de/thm/mni/thmtimer/model/TimeTracking.java
+++ src/de/thm/mni/thmtimer/model/TimeTracking.java
@@ -1,50 +1,37 @@
 package de.thm.mni.thmtimer.model;
 
-
+import de.thm.mni.thmtimer.util.StaticModuleData;
 
 public class TimeTracking {
-	
+
 	private Long m_id;
 	private Long m_categoryID;
-	private String m_description;
 	private TimeData m_time;
-	
-	
+
 	public TimeTracking(Long id, Long categoryID, String description, TimeData time) {
-		
+
 		m_id = id;
 		m_categoryID = categoryID;
-		m_description = description;
 		m_time = time;
 	}
-	
-	
-	
+
 	public Long getID() {
-		
+
 		return m_id;
 	}
-	
-	public String getDescription() {
-		
-		return m_description;
-	}
-	
+
 	public Long getCategoryID() {
-		
+
 		return m_categoryID;
 	}
-	
+
 	public TimeData getTime() {
-		
+
 		return m_time;
 	}
-	
-	
 
 	@Override
 	public String toString() {
-		
-		return m_description + " (" + m_time.getTimeInMinutes().toString() + " min)";
+		return StaticModuleData.findTimeCategory(m_categoryID).toString() + " (" + m_time.getTimeStringHHMM() + ")";
 	}
 }
