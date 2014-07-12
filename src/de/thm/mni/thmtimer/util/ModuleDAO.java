c4336f30bc5673c99e178a8211a78e30607614c9
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2014-07-12 21:48:42 +0200

    Remove unused import

--- src/de/thm/mni/thmtimer/util/ModuleDAO.java
+++ src/de/thm/mni/thmtimer/util/ModuleDAO.java
@@ -2,7 +2,6 @@ package de.thm.mni.thmtimer.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.http.HttpMethod;
